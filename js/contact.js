47d73fa9c8e12488da78305353ba6f6098895802
EivindLan <Eivind.grimstvedt.langeland@gmail.com>
2022-12-14 19:40:21 +0100

    Update contact.js
    Second commit

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,5 @@
 $("document").ready(function createArray1() {
     let Info1 = new Array("email", "telephone_number", "address");
-    console.log(Info1);
-    
 });
 
 $("document").ready(function InfoBtn() {
@@ -10,31 +8,30 @@ $("document").ready(function InfoBtn() {
 let Email = window.prompt("What is your e-mail?");
   if(Email.includes("@", ".")){
     let PhnNr = prompt("What is your phone number?")
-    if(isNaN(PhnNr) || PhnNr < 0){
-        //if value is not correct (is non a number, or lower than 0) reprompt value   
+    if(isNaN(PhnNr) || PhnNr < 0){  
         PhnNr = prompt("Please write a valid phone number");
-        }
-        //check the new re-prompt. 
+    }   
+        //since this is in the middle of several prompts it would be annoying to start over
         if(isNaN(PhnNr) || PhnNr < 0){
             alert("Your phone number is not valid, try again.");
         }
-        // as the adress can be delivered in many formats I wont put any restrictions on it.
-        else {   
-        var Adr = window.prompt("What is your adress?");
+        // as the address can be delivered in many formats I wont put any restrictions on it.
+        else  {   
+        var Adr = window.prompt("What is your address?");
         window.alert("thank you, I will be contacting you");
         let Info1 = [Email, PhnNr, Adr];
         console.log(Info1);
         console.log("From: " + Email);
         console.log("Contact: " + PhnNr);
         console.log("Address: " + Adr);
-          }
-       }
-    
+        }
+  }
 
-else {
+    else {
     alert("Your e-mail is not valid, try again.")
     };
-});
+  
+  });
 });
 
 
